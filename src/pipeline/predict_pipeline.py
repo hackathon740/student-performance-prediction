e2c28154841efd5d4c0ad61d334143a9079435fa
Abhishek Chattaraj <abhishekchattaraj4@gmail.com>
2026-06-19 00:12:46 +0530

    fix predict pipeline

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import pandas as pd
 from src.exception import CustomException
@@ -9,19 +10,38 @@ class PredictPipeline:
         pass
 
     def predict(self,features):
-        try:
-            model_path=os.path.join("artifacts","model.pkl")
-            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
-            print("Before Loading")
-            model=load_object(file_path=model_path)
-            preprocessor=load_object(file_path=preprocessor_path)
-            print("After Loading")
-            data_scaled=preprocessor.transform(features)
-            preds=model.predict(data_scaled)
-            return preds
-        
-        except Exception as e:
-            raise CustomException(e,sys)
+    try:
+        print("STEP 1")
+
+        model_path=os.path.join("artifacts","model.pkl")
+        preprocessor_path=os.path.join("artifacts","preprocessor.pkl")
+
+        print(model_path)
+        print(preprocessor_path)
+
+        print("STEP 2")
+
+        model=load_object(file_path=model_path)
+
+        print("MODEL LOADED")
+
+        preprocessor=load_object(file_path=preprocessor_path)
+
+        print("PREPROCESSOR LOADED")
+
+        data_scaled=preprocessor.transform(features)
+
+        print("DATA TRANSFORMED")
+
+        preds=model.predict(data_scaled)
+
+        print("PREDICTION DONE")
+
+        return preds
+
+    except Exception as e:
+        print("ERROR:",e)
+        raise CustomException(e,sys)
 
 
 
