a5c75eb970252ad2f1a04fbda7a62fee3ec03e33
Abhishek Chattaraj <abhishekchattaraj4@gmail.com>
2026-06-18 20:45:19 +0530

    updated code

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
 name='mlproject',
 version='0.0.1',
-author='Krish',
-author_email='krishnaik06@gmail.com',
+author='Abhishek',
+author_email='abhishekchattaraj4@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
